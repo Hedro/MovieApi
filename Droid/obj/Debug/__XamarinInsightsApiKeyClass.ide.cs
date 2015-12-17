74fd2d1632f0b44a1623dcabe5466610c9947ec8
Boksic <boksic.rodrigo@ynov.com>
2015-12-17 11:24:59 +0100

    Hello world

---
+++ Droid/obj/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace MovieApi.Droid
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "a0ae6e2891a402e60724a021060f993a09e3db3e";
+	}
+}
---
+++ iOS/obj/iPhoneSimulator/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace MovieApi.iOS
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "a0ae6e2891a402e60724a021060f993a09e3db3e";
+	}
+}
